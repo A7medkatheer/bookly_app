7a3a09904b7744d046e703d7dfbbec435d945209
katheer <[email protected]>
2024-07-15 21:00:23 +0300

    refactor book details view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,10 +1,7 @@
 import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/book_action.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_details_section.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/similer_books_list_view.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/similer_books_section.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -12,8 +9,7 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    return CustomScrollView(
+    return const CustomScrollView(
       slivers: [
         SliverFillRemaining(
           hasScrollBody: false,
@@ -21,44 +17,12 @@ class BookDetailsViewBody extends StatelessWidget {
             padding: edgeInsets,
             child: Column(
               children: [
-                const CustomBookDetailsAppBar(),
-                const SizedBox(height: 20),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: width * 0.2),
-                  child: const CustomBookItem(),
-                ),
-                const SizedBox(height: 43),
-                Text(
-                  "The Jungle Book",
-                  style: Styles.textStyle30.copyWith(
-                      fontWeight: FontWeight.bold, fontFamily: kGtSectraFine),
-                ),
-                const SizedBox(height: 3),
-                Opacity(
-                  opacity: 0.7,
-                  child: Text(
-                    "Rudyard Kipling",
-                    style: Styles.textStyle18.copyWith(
-                        fontStyle: FontStyle.italic,
-                        fontWeight: FontWeight.w500,
-                        fontFamily: kGtSectraFine),
-                  ),
-                ),
-                const BookRating(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                ),
-                const SizedBox(height: 37),
-                const BookAction(),
-                const Expanded(child: SizedBox(height: 50)),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text("You can also like",
-                      style: Styles.textStyle16.copyWith(
-                        fontWeight: FontWeight.bold,
-                      )),
-                ),
-                const SizedBox(height: 16),
-                const SimilerBooksListView(),
+                CustomBookDetailsAppBar(),
+                SizedBox(height: 20),
+                BookDetailsSection(),
+                Expanded(child: SizedBox(height: 50)),
+                SimilerBooksSection(),
+                SizedBox(height: 40),
               ],
             ),
           ),
