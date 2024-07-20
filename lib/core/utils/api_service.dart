3bd7783404ab5c7d61a469c474d2a8b468311663
katheer <[email protected]>
2024-07-20 11:27:59 +0300

    create featured books cubit state

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+
+ final String _baseUrl = 'https://www.googleapis.com/books/v1/';
+final Dio _dio;
+
+  ApiService({required Dio dio}) : _dio = dio; 
+  Future <Map<String,dynamic>>get({required String endPoint}) async {
+    try {
+      final response = await _dio.get('$_baseUrl$endPoint');
+      return response.data;
+    } catch (e) {
+      rethrow;
+    }
+  }
+ 
+}
