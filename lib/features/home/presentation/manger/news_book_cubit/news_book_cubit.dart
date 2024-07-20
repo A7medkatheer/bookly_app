07e1536e08e9755988ccf16a0f74ac9c3f8be8cd
katheer <[email protected]>
2024-07-20 11:40:00 +0300

    news books cubit done

--- lib/features/home/presentation/manger/news_book_cubit/news_book_cubit.dart
+++ lib/features/home/presentation/manger/news_book_cubit/news_book_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'news_book_state.dart';
 
 class NewsBookCubit extends Cubit<NewsBookState> {
-  NewsBookCubit() : super(NewsBookInitial());
+  NewsBookCubit(this.homeRepo) : super(NewsBookInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(NewsBookLoading());
+    try {
+      final books = await homeRepo.fetchNewsBooks();
+      emit(books.fold(
+        (failure) => NewsBookFailure(errMessage: failure.toString()),
+        (bookList) => NewsBookSuccess(books: bookList),
+      ));
+    } catch (e) {
+      emit(NewsBookFailure(errMessage: e.toString()));
+    }
+  }
 }
